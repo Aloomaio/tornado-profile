acc9dbe193b77b147d5703353c7921b05e7bb1ae
shani-einav <shani@alooma.io>
2018-02-18 17:35:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,12 @@ setup(
     long_description=open("README.rst").read(),
     license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Topic :: Utilities",
